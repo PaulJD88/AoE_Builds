c0b6e779251ffe87d3c348bbff68fd6c36dd069a
PaulJD88 <77843480+PaulJD88@users.noreply.github.com>
2021-11-17 21:03:53 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/builds', (req, res) => {
+    res.send([
+        {
+            id: 1,
+            name: "Turtling",
+            subheading: "How to defend effectively",
+            url: "https://google.com"
+        },
+        {
+            id: 2,
+            name: "Rush",
+            subheading: "How to rush effectively",
+            url: "https://diamondlobby.com/age-of-empires-4/english-longbow-rush-aoe4/"
+        },
+        {
+            id: 3,
+            name: "Turtling",
+            subheading: "How to defend effectively",
+            url: "https://diamondlobby.com/age-of-empires-4/english-longbow-rush-aoe4/"
+        },
+        {
+            id: 4,
+            name: "Rush",
+            subheading: "How to rush effectively",
+            url: "https://diamondlobby.com/age-of-empires-4/english-longbow-rush-aoe4/"
+        },
+    ])
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
